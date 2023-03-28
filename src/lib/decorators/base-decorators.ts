4918cf3a3b0782033a6b15fe333ba1e682f1ebb2
Jeroen Claassens <support@favware.tech>
2023-03-29 00:08:02 +0200

    chore: derp, asset redirection in dev mode

--- src/lib/decorators/base-decorators.ts
+++ src/lib/decorators/base-decorators.ts
@@ -3,7 +3,21 @@ import type { ClassWithName, EnumerableCacheEntry, EnumerableClassReturnType, En
 
 const enumerableCache: Map<string, EnumerableCacheEntry[]> = new Map();
 
+/**
+ * Decorator that reads {@link enumerableCache} for the decorated class and sets the enumerable property of any fields decorated with {@link Enumerable}.
+ *
+ * Without applying this decorator on the class the field {@link Enumerable} decorators will have no effect!
+ * @param value Whether the property should be enumerable or not
+ */
 export function Enumerable<Class extends PieceConstructor>(): EnumerableClassReturnType<Class>;
+/**
+ * Decorator that queues the decorated field for being marked as `enumerable` or not based on the parameter value.
+ * Apply to the class by adding {@link Enumerable} on the class
+ *
+ * Without applying this decorator on the class {@link Enumerable} decorators on the fields will have no effect!
+ *
+ * @param value Whether the property should be enumerable or not
+ */
 export function Enumerable<This, Value>(enumerable: boolean): EnumerableFieldReturnType<This, Value>;
 export function Enumerable<ThisOrClass, Value>(enumerable?: boolean) {
 	if (typeof enumerable === 'boolean') {
